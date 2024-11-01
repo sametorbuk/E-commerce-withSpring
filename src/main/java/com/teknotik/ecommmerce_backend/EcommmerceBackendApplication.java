7529611eccedefdda4d94ec7cce38bfc7c808390
Samet Orbuk <orbuk60@gmail.com>
2024-11-01 19:26:29 +0300

    İlk commit

---
+++ src/main/java/com/teknotik/ecommmerce_backend/EcommmerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teknotik.ecommmerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommmerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommmerceBackendApplication.class, args);
+	}
+
+}
