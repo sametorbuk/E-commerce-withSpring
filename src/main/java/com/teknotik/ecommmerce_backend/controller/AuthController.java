3a42a55a99b9667bd19a0f2bc63cef1618445afe
Samet Orbuk <orbuk60@gmail.com>
2024-11-04 19:33:53 +0300

    register method made enabled

--- src/main/java/com/teknotik/ecommmerce_backend/controller/AuthController.java
+++ src/main/java/com/teknotik/ecommmerce_backend/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.teknotik.ecommmerce_backend.controller;
 
 
 import com.teknotik.ecommmerce_backend.dto.RegisterResponse;
+import com.teknotik.ecommmerce_backend.dto.RegistrationStore;
 import com.teknotik.ecommmerce_backend.dto.RegistrationUser;
 import com.teknotik.ecommmerce_backend.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +18,34 @@ public class AuthController {
     private AuthenticationService authenticationService;
 
 
+
+    @Autowired
+    public AuthController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+
+
     @PostMapping("/register")
     public RegisterResponse register(@RequestBody RegistrationUser registrationUser) {
         switch (registrationUser.roleId()) {
             case 1:
                 authenticationService.adminRegister(registrationUser.name(), registrationUser.email(), registrationUser.password());
                 return new RegisterResponse(registrationUser.email(), "Registration happened successfully");
-            case 2:
-
             case 3:
                 authenticationService.customerRegister(registrationUser.name(), registrationUser.email(), registrationUser.password());
                 return new RegisterResponse(registrationUser.email(), "Registration happened successfully");
         }
         throw new RuntimeException();
     }
+
+    @PostMapping("/storeRegister")
+    public RegisterResponse storeRegister(@RequestBody RegistrationStore registrationStore){
+        authenticationService.storeRegister(registrationStore.name(), registrationStore.email(),
+                registrationStore.password(), registrationStore.storeName(),
+                registrationStore.storePhone(), registrationStore.storeTaxId(), registrationStore.storeBankAccount());
+        return new RegisterResponse(registrationStore.email(), "Registration happened successfully");
+    }
+
+
 }
---
+++ src/main/java/com/teknotik/ecommmerce_backend/dto/RegistrationStore.java
@@ -0,0 +1,4 @@
+package com.teknotik.ecommmerce_backend.dto;
+
+public record RegistrationStore(String name, String email , String password,String storeName,String storePhone,String storeTaxId, String storeBankAccount) {
+}
