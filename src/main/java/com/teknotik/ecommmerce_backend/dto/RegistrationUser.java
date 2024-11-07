f3ebb99659e0782aa134481dd9490833db19ccd8
Samet Orbuk <orbuk60@gmail.com>
2024-11-07 13:30:30 +0300

    render deploy and register path tested

--- src/main/java/com/teknotik/ecommmerce_backend/dto/RegistrationUser.java
+++ src/main/java/com/teknotik/ecommmerce_backend/dto/RegistrationUser.java
@@ -1,4 +1,9 @@
 package com.teknotik.ecommmerce_backend.dto;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public record RegistrationUser(String name , String email , String password , int roleId) {
 }
+
+
