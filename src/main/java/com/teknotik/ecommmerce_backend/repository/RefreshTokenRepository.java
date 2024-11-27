4020427ef6fd9e4826a053c64c966fceb40fcf21
Samet Orbuk <orbuk60@gmail.com>
2024-11-27 08:53:40 +0300

    refresh token entity added

--- src/main/java/com/teknotik/ecommmerce_backend/repository/RefreshTokenRepository.java
+++ src/main/java/com/teknotik/ecommmerce_backend/repository/RefreshTokenRepository.java
@@ -10,10 +10,10 @@ import java.util.Optional;
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken , Long> {
 
-    Optional<RefreshToken> findByToken();
+    Optional<RefreshToken> findByToken(String token);
 
     void deleteByUser(User user);
 
-    Optional<RefreshToken> findByUser();
+    Optional<RefreshToken> findByUser(User user);
 
 }
--- src/main/java/com/teknotik/ecommmerce_backend/service/RefreshTokenService.java
+++ src/main/java/com/teknotik/ecommmerce_backend/service/RefreshTokenService.java
@@ -1,9 +1,15 @@
 package com.teknotik.ecommmerce_backend.service;
 
+import com.teknotik.ecommmerce_backend.entity.RefreshToken;
+import com.teknotik.ecommmerce_backend.entity.User;
 import com.teknotik.ecommmerce_backend.repository.RefreshTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class RefreshTokenService {
@@ -19,6 +25,24 @@ public class RefreshTokenService {
     public RefreshTokenService(UserServiceImpl userService, RefreshTokenRepository refreshTokenRepository) {
         this.userService = userService;
         this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+
+    public RefreshToken createRefreshToken(User user){
+        refreshTokenRepository.deleteByUser(user);
+
+        RefreshToken refreshToken = new RefreshToken();
+
+        refreshToken.setUser(user);
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setExpireDate(LocalDateTime.now().plusDays(refreshTokenDurationMs));
+
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+
+    public Optional<RefreshToken> findByToken(String token){
+        return refreshTokenRepository.findByToken(token);
     }
 
 
