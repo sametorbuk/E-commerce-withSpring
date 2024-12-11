f1604a46d8d4789cd0ae9892d574e5c498641944
Samet Orbuk <orbuk60@gmail.com>
2024-12-11 10:34:45 +0300

    imported dependencies cleaned

--- src/main/java/com/teknotik/ecommmerce_backend/Util/JwtAuthenticationFilter.java
+++ src/main/java/com/teknotik/ecommmerce_backend/Util/JwtAuthenticationFilter.java
@@ -1,27 +1,20 @@
 package com.teknotik.ecommmerce_backend.Util;
-
-import com.teknotik.ecommmerce_backend.Util.JwtUtil;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
